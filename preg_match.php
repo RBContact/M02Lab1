4d3781b675b06a20c62034ad7016415e1a90c5b8
RBContact <49795695+RBContact@users.noreply.github.com>
2022-04-05 15:00:14 -0500

    Update preg_match.php

--- preg_match.php
+++ preg_match.php
@@ -1,5 +1,5 @@
 <?php
     $urlinput = "http://www.sdev253.net/home.html";
     preg_match_all ("/www.(.*)\//", $urlinput, $pat_array);
-    print $pat_array[1][0];
+    print "Your Domain Name is ".$pat_array[1][0];
 ?>
